7f6892af8bf6b7c5c770b8154c75923e2177508e
deeja-ub <tomeldendjaninetara@gmail.com>
2024-08-17 15:02:19 +0800

    Edit misspelled variable in utils ts

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,17 +1,9 @@
-import React from 'react'
 import AuthForm from '@/components/AuthForm'
-import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 const SignUp = async () => {
-  const loggedInUser = await getLoggedInUser();
-
-  console.log(loggedInUser);
-  
   return (
-    <section className='flex-center size-full max-sm:px-6'>
-      <AuthForm 
-        type="sign-up"
-      />
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
     </section>
   )
 }
